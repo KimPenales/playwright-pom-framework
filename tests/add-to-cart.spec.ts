58f93175c1b9073359d42aa81eb9abbbc05dcbfe
KimPenales <albertpenales22@gmail.com>
2026-06-12 01:10:58 +0800

    Refactored spec.ts to add test describe

--- tests/add-to-cart.spec.ts
+++ tests/add-to-cart.spec.ts
@@ -2,29 +2,33 @@ import { test } from '@playwright/test';
 import { LoginPage } from '../pages/login-page';
 import { InventoryPage } from '../pages/inventory-page';
 import { users } from '../test-data/user-data';
+import { CartPage } from '../pages/cart-page';
 
-test.beforeEach(async ({page}) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.gotoLoginPage();
-    await loginPage.login(
-        users.standardUser.username,
-        users.standardUser.password
-    );
-});
+test.describe('Inventory Features', () => {
+    test.beforeEach(async ({page}) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.gotoLoginPage();
+        await loginPage.login(
+            users.standardUser.username,
+            users.standardUser.password
+        );
+    });
 
-test('user can add multiple products to cart', async ({ page }) => {
-    const inventoryPage = new InventoryPage(page);
-    
-    //3. Validate inventory page is loaded
-    await inventoryPage.validateInventoryPageLoaded();
-    //4. Add products
-    await inventoryPage.addMultipleProductsToCart();
-    //5. Validate remove buttons
-    await inventoryPage.validateRemoveButtons();
-    //6. Validate cart count
-    await inventoryPage.validateCartBadge('3');
-    //7. Open cart
-    await inventoryPage.openCart();
-    //8. Validate cart page is loaded
-    await inventoryPage.validateCartPageLoaded();
+    test('user can add multiple products to cart', async ({ page }) => {
+        const inventoryPage = new InventoryPage(page);
+        const cartPage = new CartPage(page);
+        
+        //3. Validate inventory page is loaded
+        await inventoryPage.validateInventoryPageLoaded();
+        //4. Add products
+        await inventoryPage.addMultipleProductsToCart();
+        //5. Validate remove buttons
+        await inventoryPage.validateRemoveButtons();
+        //6. Validate cart count
+        await inventoryPage.validateCartBadge('3');
+        //7. Open cart
+        await inventoryPage.openCart();
+        //8. Validate cart page is loaded
+        await cartPage.validateCartPageLoaded();
+    });
 });
--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -4,31 +4,33 @@ import { LoginPage } from '../pages/login-page';
 import { InventoryPage } from '../pages/inventory-page';
 import { CartPage } from '../pages/cart-page';
 
-test.beforeEach(async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.gotoLoginPage();
-    await loginPage.login(
-        users.standardUser.username,
-        users.standardUser.password
-    );
-});
+test.describe('Cart Features', () => {
+    test.beforeEach(async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.gotoLoginPage();
+        await loginPage.login(
+            users.standardUser.username,
+            users.standardUser.password
+        );
+    });
 
-test('user can validate products in cart', async ({ page }) => {
-    const inventoryPage = new InventoryPage(page);
-    const cartPage = new CartPage(page);
- 
-    //3. Validate inventory page is loaded
-    await inventoryPage.validateInventoryPageLoaded();
-    //4. Add products
-    await inventoryPage.addMultipleProductsToCart();
-    //5. Validate remove buttons
-    await inventoryPage.validateRemoveButtons();
-    //6. Validate cart count
-    await inventoryPage.validateCartBadge('3');
-    //7. Open cart
-    await inventoryPage.openCart();
-    //8. Validate cart page is loaded
-    await cartPage.validateCartPageLoaded();
-    //9. Validate added products
-    await cartPage.validateProductsInCart();
+    test('user can validate products in cart', async ({ page }) => {
+        const inventoryPage = new InventoryPage(page);
+        const cartPage = new CartPage(page);
+    
+        //3. Validate inventory page is loaded
+        await inventoryPage.validateInventoryPageLoaded();
+        //4. Add products
+        await inventoryPage.addMultipleProductsToCart();
+        //5. Validate remove buttons
+        await inventoryPage.validateRemoveButtons();
+        //6. Validate cart count
+        await inventoryPage.validateCartBadge('3');
+        //7. Open cart
+        await inventoryPage.openCart();
+        //8. Validate cart page is loaded
+        await cartPage.validateCartPageLoaded();
+        //9. Validate added products
+        await cartPage.validateProductsInCart();
+    });
 });
